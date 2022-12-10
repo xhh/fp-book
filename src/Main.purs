d94af4916cc7278b0f899c7b45d9cb3fcbfc997d
xhh <159740+xhh@users.noreply.github.com>
2022-12-10 17:43:59 +0800

    findLastIndex

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,28 @@ module Main where
 
 import Prelude
 
+import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  log $ show $ findLastIndex (_ == 10) (Nil :: List Int)
+  log $ show $ findLastIndex (_ == 10) (10 : 5 : 10 : -1 : 2 : 10 : Nil)
+  log $ show $ findLastIndex (_ == 10) (11 : 12 : Nil)
+
+findLastIndex :: ∀ a. (a -> Boolean) -> List a -> Maybe Int
+findLastIndex pred l = go 0 l
+  where
+  go :: Int -> List a -> Maybe Int
+  go _ Nil = Nothing
+  go i (x : Nil)
+    | pred x = Just i
+    | otherwise = Nothing
+  -- go i (x : Nil) =
+  --   if pred x then
+  --     Just i
+  --   else
+  --     Nothing
+  go i (_ : xs) = go (i + 1) xs
